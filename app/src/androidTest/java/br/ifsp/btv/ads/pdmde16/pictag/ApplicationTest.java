06d009ee1212f7770d3eb6bef60b465cd21de645
Lucas <Lucas>
2016-06-16 20:20:35 -0300

    Agora vai.

---
+++ app/src/androidTest/java/br/ifsp/btv/ads/pdmde16/pictag/ApplicationTest.java
@@ -0,0 +1,13 @@
+package br.ifsp.btv.ads.pdmde16.pictag;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
