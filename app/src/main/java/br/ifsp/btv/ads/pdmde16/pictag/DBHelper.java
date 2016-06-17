2c11e0fb8a9ce7ac50fa3af3ea451fdf7706941c
Davi Aragão <davi.aragao@cpb.com.br>
2016-06-16 21:34:59 -0300

    tagDAO e tag on DBHelper

--- app/src/main/java/br/ifsp/btv/ads/pdmde16/pictag/DBHelper.java
+++ app/src/main/java/br/ifsp/btv/ads/pdmde16/pictag/DBHelper.java
@@ -13,10 +13,20 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final String CREATE_TABLE_FOTO =
             "CREATE TABLE FOTO (" +
                     "id INT NOT NULL AUTO_INCREMENT, " +
-                    "caminho VARCHAR(100) NOT NULL);";
+                    "caminho VARCHAR(100) NOT NULL," +
+                    "primary key(id));";
 
     public static final String DROP_TABLE_FOTO =
             "DROP TABLE IF EXISTS FOTO;";
+
+    public static final String CREATE_TABLE_TAG =
+            "CREATE TABLE TAG(" +
+                "id INT NOT NULL AUTO_INCREMENT, " +
+                "nome VARCHAR(100) NOT NULL, " +
+                "primary key(id));";
+
+    public static final String DROP_TABLE_TAG =
+            "DROP TABLE IF EXISTS TAG;";
 
     public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -29,11 +39,13 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_TABLE_FOTO);
+        db.execSQL(CREATE_TABLE_TAG);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL(DROP_TABLE_FOTO);
+        db.execSQL(DROP_TABLE_TAG);
         onCreate(db);
     }
 }
