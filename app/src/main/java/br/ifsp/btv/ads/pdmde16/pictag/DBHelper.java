71829bd8f30e02e1fecbb0b24553371dab750be7
Davi Aragão <davi.aragao@cpb.com.br>
2016-06-23 22:08:24 -0300

    fimDaAula

--- app/src/main/java/br/ifsp/btv/ads/pdmde16/pictag/DBHelper.java
+++ app/src/main/java/br/ifsp/btv/ads/pdmde16/pictag/DBHelper.java
@@ -10,19 +10,18 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final Integer VERSAO_BANCO = 1;
 
     public static final String CREATE_TABLE_FOTO =
-            "CREATE TABLE FOTO (" +
-                    "id INT NOT NULL AUTO_INCREMENT, " +
-                    "caminho VARCHAR(100) NOT NULL," +
-                    "primary key(id));";
+            "CREATE TABLE FOTO(" +
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "caminho VARCHAR(100) NOT NULL);";
+
 
     public static final String DROP_TABLE_FOTO =
             "DROP TABLE IF EXISTS FOTO;";
 
     public static final String CREATE_TABLE_TAG =
             "CREATE TABLE TAG(" +
-                "id INT NOT NULL AUTO_INCREMENT, " +
-                "nome VARCHAR(100) NOT NULL, " +
-                "primary key(id));";
+                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "nome VARCHAR(100) NOT NULL);";
 
     public static final String DROP_TABLE_TAG =
             "DROP TABLE IF EXISTS TAG;";
--- app/src/main/java/br/ifsp/btv/ads/pdmde16/pictag/MainActivity.java
+++ app/src/main/java/br/ifsp/btv/ads/pdmde16/pictag/MainActivity.java
@@ -1,15 +1,23 @@
 package br.ifsp.btv.ads.pdmde16.pictag;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TableLayout;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TableLayout tblTags;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,33 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        TagDAO tDAO = new TagDAO(getApplicationContext());
+        tDAO.createTag("#davi");
+        tDAO.createTag("#aragao");
+        atualizarTags();
+    }
+
+    private void atualizarTags() {
+        TagDAO tDAO = new TagDAO(getApplicationContext());
+        List<String> tags = tDAO.getAll();
+
+        for (int i = 0; i < tags.size(); i += 2)
+            criarTag(tags.get(i), tags.get(i + 1));
+    }
+
+    private void criarTag(String tag1, String tag2) {
+        LayoutInflater inflador = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+
+        View novaTag = inflador.inflate(R.layout.layout_tag, null);
+
+        Button btnTag1 = (Button) novaTag.findViewById(R.id.btnTag1);
+        btnTag1.setText(tag1);
+        //btnTag1.setOnClickListener(ouvidorTag1);
+
+        Button btnTag2 = (Button) novaTag.findViewById(R.id.btnTag2);
+        btnTag2.setText(tag2);
+       // btnTag2.setOnClickListener(ouvidorTag2);
     }
 
     @Override
